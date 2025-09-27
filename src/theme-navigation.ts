c10fbc642b731e81697ef95c9cde91e1257d77a1
rafaelhgreco <zrafaelgreco@gmail.com>
2025-09-27 15:26:39 -0300

    refactor: clean up comments and improve theme navigation code readability

--- src/theme-navigation.ts
+++ src/theme-navigation.ts
@@ -1,25 +1,22 @@
-// src/navigationTheme.ts
 import { DefaultTheme, DarkTheme } from '@react-navigation/native';
 import {
   defaultTheme as myAppTheme,
   darkTheme as myAppDarkTheme,
 } from './theme';
 export const navDefaultTheme = {
-  ...DefaultTheme, // Pega o tema padrão da navegação como base
+  ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    // Sobrescreve as cores com as do seu tema Emotion
-    background: myAppTheme.colors.background, // Cor de fundo principal do app        // Cor de fundo dos "cards" (telas)
-    text: myAppTheme.colors.text, // Cor do texto
-    primary: myAppTheme.colors.primary, // Cor primária (usada em botões, abas, etc.)
+    background: myAppTheme.colors.background,
+    text: myAppTheme.colors.text,
+    primary: myAppTheme.colors.primary,
   },
 };
 
 export const navDarkTheme = {
-  ...DarkTheme, // Pega o tema escuro da navegação como base
+  ...DarkTheme,
   colors: {
     ...DarkTheme.colors,
-    // Sobrescreve as cores com as do seu tema Emotion
     background: myAppDarkTheme.colors.background,
     text: myAppDarkTheme.colors.text,
     primary: myAppDarkTheme.colors.primary,
